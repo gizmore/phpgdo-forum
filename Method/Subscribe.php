46a9b9ac6789813b92d2d0787bac311888393141
gizmore <gizmore@wechall.net>
2022-09-01 11:15:40 +0200

    '7.0.1-r1560 tests'

--- Method/Subscribe.php
+++ Method/Subscribe.php
@@ -5,53 +5,60 @@ use GDO\Core\Method;
 use GDO\Core\Website;
 use GDO\Forum\GDO_ForumBoard;
 use GDO\Forum\GDO_ForumBoardSubscribe;
-use GDO\Forum\GDO_ForumThread;
 use GDO\Forum\GDO_ForumThreadSubscribe;
 use GDO\User\GDO_User;
-use GDO\Util\Common;
 use GDO\Forum\Module_Forum;
+use GDO\Core\GDT_Object;
+use GDO\Forum\GDO_ForumThread;
 
 /**
  * Subscribe boards or thread
  * Guest user is not allowed, because he does not have an email.
+ * 
  * @author gizmore
  */
 final class Subscribe extends Method
 {
 	public function isUserRequired() : bool { return true; }
 	
+	public function isGuestAllowed() : bool { return false; }
+	
+	public function gdoParameters() : array
+	{
+		return [
+			GDT_Object::make('board')->table(GDO_ForumBoard::table()),
+			GDT_Object::make('thread')->table(GDO_ForumThread::table()),
+		];
+	}
+	
     public function execute()
     {
         $user = GDO_User::current();
         
-        if ($boardId = Common::getRequestString('board'))
+        if ($boardId = $this->gdoParameterVar('board'))
         {
             if ($boardId == Module_Forum::instance()->cfgRootID())
             {
                 return $this->error('err_please_use_subscribe_all');
             }
-            $board = GDO_ForumBoard::findById($boardId);
             GDO_ForumBoardSubscribe::blank(array(
                 'subscribe_user' => $user->getID(),
                 'subscribe_board' => $boardId,
             ))->replace();
             $user->tempUnset('gdo_forum_board_subsciptions');
             $user->recache();
-            $href = href('Forum', 'Boards', '&board='.$board->getParent()->getID());
-            return Website::redirectMessage('msg_subscribed', null, $href);
+            return $this->redirectMessage('msg_subscribed');
         }
         
-        elseif ($threadId = Common::getRequestString('thread'))
+        elseif ($threadId = $this->gdoParameterVar('thread'))
         {
-            $thread = GDO_ForumThread::findById($threadId);
-            GDO_ForumThreadSubscribe::blank(array(
+            GDO_ForumThreadSubscribe::blank([
                 'subscribe_user' => $user->getID(),
                 'subscribe_thread' => $threadId,
-            ))->replace();
+            ])->replace();
             $user->tempUnset('gdo_forum_thread_subsciptions');
             $user->recache();
-            $href = Website::hrefBack(href('Forum', 'Boards', '&board='.$thread->getBoard()->getID()));
-            return Website::redirectMessage('msg_subscribed', null, $href);
+            return $this->redirectMessage('msg_subscribed');
         }
     }
 
