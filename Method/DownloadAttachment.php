923c9cb43ef7ab291fa320a34723d5b2bfbcb935
gizmore <gizmore@wechall.net>
2022-10-07 04:35:34 +0200

    'Privacy and SSL'

--- Method/DownloadAttachment.php
+++ Method/DownloadAttachment.php
@@ -6,6 +6,7 @@ use GDO\Forum\GDO_ForumPost;
 use GDO\User\GDO_User;
 use GDO\Util\Common;
 use GDO\File\Method\GetFile;
+use GDO\Forum\GDT_ForumPost;
 
 /**
  * Download a post attachment.
@@ -17,11 +18,28 @@ final class DownloadAttachment extends Method
 {
     public function isSavingLastUrl() : bool { return false; }
     
+    public function getMethodTitle() : string
+    {
+    	return t('download');
+    }
+    
+    public function gdoParameters() : array
+    {
+    	return [
+    		GDT_ForumPost::make('post')->notNull(),
+    	];
+    }
+    
+    public function getPost() : GDO_ForumPost
+    {
+    	return $this->gdoParameterValue('post');
+    }
+    
     public function execute()
     {
         $user = GDO_User::current();
         $table = GDO_ForumPost::table();
-        $post = $table->find(Common::getRequestString('post'));
+        $post = $this->getPost();
         if (!$post->canView($user))
         {
             return $this->error('err_permission_read');
